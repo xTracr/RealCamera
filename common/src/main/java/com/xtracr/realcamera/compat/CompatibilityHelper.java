ce0914b1e8e4c6a808d01624ba7bfd284c21c3bc
H31M5 <2211923909@qq.com>
2025-09-02 13:19:42 +0800

    Try to be compatible with TACZ (#154)

--- common/src/main/java/com/xtracr/realcamera/compat/CompatibilityHelper.java
+++ common/src/main/java/com/xtracr/realcamera/compat/CompatibilityHelper.java
@@ -5,6 +5,9 @@ import com.xtracr.realcamera.RealCameraCore;
 import com.xtracr.realcamera.config.ConfigFile;
 import com.xtracr.realcamera.mixin.accessor.CameraAccessor;
 import net.minecraft.client.Camera;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.entity.player.Player;
 
 import java.lang.reflect.Method;
 
@@ -12,6 +15,8 @@ public class CompatibilityHelper {
     private static PlatformHelper platformHelper;
     private static Class<?> NEA_NEAnimationsLoader;
     private static Method NEA_playerTransformer_setDeltaTick;
+    private static Class<?> TACZ_IClientPlayerGunOperator;
+    private static Method TACZ_IClientPlayerGunOperator_fromLocalPlayer;
 
     public static void initialize(PlatformHelper platformHelper) {
         CompatibilityHelper.platformHelper = platformHelper;
@@ -22,6 +27,25 @@ public class CompatibilityHelper {
             NEA_playerTransformer_setDeltaTick = NEA_PlayerTransformer.getDeclaredMethod("setDeltaTick", float.class);
         } catch (Exception exception) {
             RealCamera.LOGGER.warn("Compatibility with Not Enough Animations is outdated: [{}] {}", exception.getClass().getName(), exception.getMessage());
+        }
+        if (isModLoaded("tacz")) try {
+            TACZ_IClientPlayerGunOperator = Class.forName("com.tacz.guns.api.client.gameplay.IClientPlayerGunOperator"); 
+            TACZ_IClientPlayerGunOperator_fromLocalPlayer = TACZ_IClientPlayerGunOperator.getMethod("fromLocalPlayer", LocalPlayer.class);
+            DisableHelper.MAIN_FEATURE.registerOrInBinding(player -> TACZ_gunsIsAiming(player));
+        } catch (Exception e) {
+            RealCamera.LOGGER.warn("TACZ is not loaded correctly: [{}] {}", e.getClass().getName(), e.getMessage());
+        }
+    }
+
+    private static boolean TACZ_gunsIsAiming(Player player) {
+        if (!(player instanceof LocalPlayer localPlayer)) return false;
+        try {
+            Object operator = TACZ_IClientPlayerGunOperator_fromLocalPlayer.invoke(null, localPlayer);
+            Method getProgressMethod = TACZ_IClientPlayerGunOperator.getMethod("getClientAimingProgress", float.class);
+            float aimingProgress = (float) getProgressMethod.invoke(operator, Minecraft.getInstance().getFrameTime());
+                return aimingProgress > 0;
+        } catch (Exception e) {
+            return false;
         }
     }
 
