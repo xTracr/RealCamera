ad58191d6bddc8ab00221450426eb77f7cbbe774
xTracr <xtracr@outlook.com>
2026-04-11 09:42:48 +0800

    Fix raycast

--- common/src/main/java/com/xtracr/realcamera/mixin/MixinLocalPlayer.java
+++ common/src/main/java/com/xtracr/realcamera/mixin/MixinLocalPlayer.java
@@ -1,6 +1,9 @@
 package com.xtracr.realcamera.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.authlib.GameProfile;
+import com.mojang.datafixers.util.Pair;
 import com.xtracr.realcamera.RealCameraCore;
 import com.xtracr.realcamera.compat.DisableHelper;
 import com.xtracr.realcamera.config.ConfigFile;
@@ -14,50 +17,50 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import org.jspecify.annotations.NonNull;
-import org.spongepowered.asm.mixin.Final;
+import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.function.Predicate;
+
 @Mixin(LocalPlayer.class)
 public abstract class MixinLocalPlayer extends AbstractClientPlayer {
-    @Shadow
-    @Final
-    protected Minecraft minecraft;
-
     public MixinLocalPlayer(ClientLevel world, GameProfile profile) {
         super(world, profile);
     }
 
-    @Inject(method = "getRopeHoldPosition", at = @At("HEAD"), cancellable = true)
-    private void realcamera$atGetRopePosHEAD(float partialTicks, CallbackInfoReturnable<Vec3> cir) {
-        if (DisableHelper.RENDER_HANDS.disabled(this)) cir.setReturnValue(super.getRopeHoldPosition(partialTicks));
+    @WrapOperation(method = "pick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/projectile/ProjectileUtil;getEntityHitResult(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/world/phys/AABB;Ljava/util/function/Predicate;D)Lnet/minecraft/world/phys/EntityHitResult;"))
+    private static @Nullable EntityHitResult realcamera$wrapGetEntityHitResult(Entity except, Vec3 from, Vec3 to, AABB box, Predicate<Entity> matching, double maxValue, Operation<EntityHitResult> original, Entity cameraEntity, double blockInteractionRange, double entityInteractionRange, float partialTicks) {
+        if (!ConfigFile.config().dynamicCrosshair() && RealCameraCore.isActive()) {
+            double interactionRange = Math.max(blockInteractionRange, entityInteractionRange);
+            Pair<Vec3, Vec3> fromAndTo = RaycastUtil.getFromAndTo(cameraEntity, interactionRange * interactionRange, partialTicks);
+            Vec3 newFrom = fromAndTo.getFirst();
+            Vec3 newTo = fromAndTo.getSecond();
+            Minecraft client = Minecraft.getInstance();
+            double sqDistance = client.hitResult != null ? client.hitResult.getLocation().distanceToSqr(newFrom) : newTo.distanceToSqr(newFrom);
+            AABB newBox = cameraEntity.getBoundingBox().expandTowards(cameraEntity.getViewVector(partialTicks).scale(interactionRange)).inflate(1.0, 1.0, 1.0);
+            CrosshairUtil.capturedEntityHitResult = ProjectileUtil.getEntityHitResult(except, newFrom, newTo, newBox, matching, sqDistance);
+            return CrosshairUtil.capturedEntityHitResult;
+        }
+        return original.call(except, from, to, box, matching, maxValue);
     }
 
-    @Inject(method = "raycastHitResult", at = @At("HEAD"), cancellable = true)
-    private void realcamera$atRaycastHitResultHEAD(float partialTicks, Entity cameraEntity, CallbackInfoReturnable<HitResult> cir) {
-        if (!ConfigFile.config().dynamicCrosshair() && RealCameraCore.isActive()) {
-            Vec3 startVec = RaycastUtil.getStartVec();
-            Vec3 endVec = RaycastUtil.getEndVec();
-            double sqDistance = (minecraft.hitResult != null ? minecraft.hitResult.getLocation().distanceToSqr(startVec) : endVec.distanceToSqr(startVec));
-            double interactionRange = Math.max(blockInteractionRange(), entityInteractionRange());
-            AABB box = cameraEntity.getBoundingBox().expandTowards(cameraEntity.getViewVector(partialTicks).scale(interactionRange)).inflate(1.0, 1.0, 1.0);
-            CrosshairUtil.capturedEntityHitResult = ProjectileUtil.getEntityHitResult(cameraEntity, startVec, endVec, box, entity -> !entity.isSpectator() && entity.isPickable(), sqDistance);
-            cir.setReturnValue(CrosshairUtil.capturedEntityHitResult);
-        }
+    @Inject(method = "getRopeHoldPosition", at = @At("HEAD"), cancellable = true)
+    private void realcamera$atGetRopePosHEAD(float partialTickTime, CallbackInfoReturnable<Vec3> cir) {
+        if (DisableHelper.RENDER_HANDS.disabled(this)) cir.setReturnValue(super.getRopeHoldPosition(partialTickTime));
     }
 
     @Override
     public @NonNull HitResult pick(double maxDistance, float partialTicks, boolean includeFluids) {
         if (!ConfigFile.config().dynamicCrosshair() && RealCameraCore.isActive()) {
-            RaycastUtil.update(this, maxDistance * maxDistance, partialTicks);
-            return level().clip(RaycastUtil.getClipContext(ClipContext.Block.OUTLINE,
-                    includeFluids ? ClipContext.Fluid.ANY : ClipContext.Fluid.NONE, this));
+            Pair<Vec3, Vec3> fromAndTo = RaycastUtil.getFromAndTo(this, maxDistance * maxDistance, partialTicks);
+            return level().clip(new ClipContext(fromAndTo.getFirst(), fromAndTo.getSecond(), ClipContext.Block.OUTLINE, includeFluids ? ClipContext.Fluid.ANY : ClipContext.Fluid.NONE, this));
         }
         return super.pick(maxDistance, partialTicks, includeFluids);
     }
--- common/src/main/java/com/xtracr/realcamera/util/RaycastUtil.java
+++ common/src/main/java/com/xtracr/realcamera/util/RaycastUtil.java
@@ -1,49 +1,31 @@
 package com.xtracr.realcamera.util;
 
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.ClipContext;
-import net.minecraft.world.level.ClipContext.Block;
-import net.minecraft.world.level.ClipContext.Fluid;
 import net.minecraft.world.phys.Vec3;
 
 public class RaycastUtil {
-    private static Vec3 startVec = Vec3.ZERO;
-    private static Vec3 endVec = Vec3.ZERO;
-
-    public static Vec3 getStartVec() {
-        return startVec;
-    }
-
-    public static Vec3 getEndVec() {
-        return endVec;
-    }
-
-    public static ClipContext getClipContext(Block shapeType, Fluid fluidHandling, Entity entity) {
-        return new ClipContext(startVec, endVec, shapeType, fluidHandling, entity);
-    }
-
     /**
      * Updates raycast start and end points ensuring:
      * <li> End point is on camera's viewing direction
      * <li> Square distance from entity's eye to end point ≤ sqDistance
      */
-    public static void update(Entity entity, double sqDistance, float partialTicks) {
+    public static Pair<Vec3, Vec3> getFromAndTo(Entity entity, double sqDistance, float partialTicks) {
         Camera camera = Minecraft.getInstance().gameRenderer.getMainCamera();
         Vec3 eyePos = entity.getEyePosition(partialTicks);
-        startVec = camera.position();
+        Vec3 from = camera.position();
         Vec3 direction = Vec3.directionFromRotation(camera.xRot(), camera.yRot());
-        Vec3 offset = startVec.subtract(eyePos);
+        Vec3 offset = from.subtract(eyePos);
         Vec3 footPoint = MathUtil.getIntersectionPoint(Vec3.ZERO, direction, offset, direction);
         if (footPoint.lengthSqr() > sqDistance) {
-            startVec = eyePos;
+            from = eyePos;
             direction = entity.getViewVector(partialTicks);
-            endVec = startVec.add(direction.scale(Math.sqrt(sqDistance)));
-            return;
+            return Pair.of(from, from.add(direction.scale(Math.sqrt(sqDistance))));
         } else if (offset.lengthSqr() > sqDistance) {
-            startVec = startVec.add(direction.scale(offset.distanceTo(footPoint) - Math.sqrt(sqDistance - footPoint.lengthSqr())));
+            from = from.add(direction.scale(offset.distanceTo(footPoint) - Math.sqrt(sqDistance - footPoint.lengthSqr())));
         }
-        endVec = eyePos.add(footPoint.add(direction.scale(Math.sqrt(sqDistance - footPoint.lengthSqr()))));
+        return Pair.of(from, eyePos.add(footPoint.add(direction.scale(Math.sqrt(sqDistance - footPoint.lengthSqr())))));
     }
 }
